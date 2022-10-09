18576cfddcc962e168c36f36d5362c61a00a929b
yuyaoowada <yuyaoowada@gmail.com>
2022-10-10 00:32:04 +0900

    create imageOn

---
+++ components/layout.js
@@ -0,0 +1,14 @@
+import Footer from "./footer";
+import Header from "./header";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Header />
+
+      <main>{children}</main>
+
+      <Footer />
+    </>
+  )
+}
