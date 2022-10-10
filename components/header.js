f883eac8835d271c063c6ceb1a1a5a647e43140d
yuyaoowada <yuyaoowada@gmail.com>
2022-10-10 16:04:55 +0900

    create css module

--- components/header.js
+++ components/header.js
@@ -1,11 +1,14 @@
 import Logo from "./logo";
 import Nav from "./nav";
+import styles from 'styles/header.module.css'
 
 export default function Header() {
   return (
     <header>
-      <Logo />
-      <Nav />
+      <div className={styles.flexContainer}>
+        <Logo boxOn />
+        <Nav />
+      </div>
     </header>
 
   )
