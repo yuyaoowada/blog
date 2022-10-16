e23452e7f082c1d51008c3b794b99aa835c0bafe
yuyaoowada <yuyaoowada@gmail.com>
2022-10-17 00:07:20 +0900

    create schedule

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,17 +1,89 @@
+import { getPostBySlug } from 'lib/api'
+import Container from 'components/container'
+import PostHeader from 'components/post-header'
 import { client } from 'lib/api'
+import Image from 'next/image'
+import { getPlaiceholder } from 'plaiceholder'
+import {
+  TwoColumn,
+  TwoColumnMain,
+  TwoColumnSidebar,
+} from 'components/two-column'
+import PostBody from 'components/post-body'
+import PostCategories from 'components/post-categories'
+import { extractText } from 'lib/extract-text'
+import Meta from 'components/meta'
 
-export default function Schedule() {
-  return <h1>記事のタイトル</h1>
+// ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from 'lib/constants'
+
+export default function Schedule({
+  title,
+  publish,
+  content,
+  eyecatch,
+  categories,
+  description,
+}) {
+  return (
+    <Container>
+      <Meta
+        pageTitle={title}
+        pageDesc={description}
+        pageImg={eyecatch.url}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.height}
+      />
+      <article>
+        <PostHeader title={title} subtitle="Blog Article" publish={publish} />
+
+        <figure>
+          <Image
+            src={eyecatch.url}
+            alt=""
+            layout="responsive"
+            width={eyecatch.width}
+            height={eyecatch.height}
+            sizes="(min-width: 1152px) 1152px, 100vw"
+            priority
+            placeholder="blur"
+            blurDataURL={eyecatch.blurDataURL}
+          />
+        </figure>
+
+        <TwoColumn>
+          <TwoColumnMain>
+            <PostBody>
+              <div dangerouslySetInnerHTML={{ __html: content }} />
+            </PostBody>
+          </TwoColumnMain>
+          <TwoColumnSidebar>
+            <PostCategories categories={categories} />
+          </TwoColumnSidebar>
+        </TwoColumn>
+      </article>
+    </Container>
+  )
 }
 
 export async function getStaticProps() {
-  const resPromise = client.get({
-    endpoint: 'blogs',
-  })
-  console.log('resPromise')
-  console.log(resPromise)
+  const slug = 'micro'
+
+  const post = await getPostBySlug(slug)
+
+  const description = extractText(post.content)
+
+  const eyecatch = post.eyecatch ?? eyecatchLocal
+
+  const { base64 } = await getPlaiceholder(eyecatch.url)
+  eyecatch.blurDataURL = base64
 
   return {
-    props: {},
+    props: {
+      title: post.title,
+      eyecatch: eyecatch,
+      categories: post.categories,
+      description: description,
+    },
   }
 }
